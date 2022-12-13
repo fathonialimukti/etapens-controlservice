1d28d0970cd851525084c3e7e91d0ec97ec7d48f
fathonialimukti <fathonialim2@gmail.com>
2022-12-13 15:48:12 +0700

    update

--- controllers/database.js
+++ controllers/database.js
@@ -12,8 +12,8 @@ export const create = async ( req, res, next ) => {
 
             // mysql -u root -p ${rootPassword} -e "create database somedb"
             await execSync( `
-                sudo mysql -e "CREATE USER ${ req.body.username }@0.0.0.0 IDENTIFIED BY '${ req.body.password }';"
-                sudo mysql -e "GRANT ALL PRIVILEGES ON ${ req.body.dbname }.* TO ${ req.body.username }@localhost;"
+                sudo mysql -e "CREATE USER ${ req.body.username }@'%' IDENTIFIED BY '${ req.body.password }';"
+                sudo mysql -e "GRANT ALL PRIVILEGES ON ${ req.body.dbname }.* TO ${ req.body.username }@'%';"
 
                 sudo mysql -e "CREATE DATABASE ${ req.body.dbname };"
 
