71f8e320f04a1f7ac9c593ce1b215153341b610a
fathonialimukti <fathonialim2@gmail.com>
2023-01-09 16:42:46 +0700

    update

--- index.js
+++ index.js
@@ -28,4 +28,3 @@ app.use( errorMiddleware )
 const port = process.env.PORT || 5000
 
 app.listen( port, () => { console.log( "app listen on port " + port ) } )
-// https.createServer( options, app ).listen( port )
